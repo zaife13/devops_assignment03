96e84a61ea2a6cb873fbea46c8d31313e831682c
Huzaifa Jamal <huzaifajamal92@gmail.com>
2023-01-09 05:44:36 -0800

    changed firefox

--- test.js
+++ test.js
@@ -4,8 +4,8 @@ const { By } = require("selenium-webdriver");
 
 const test = async () => {
   const driver = new webdriver.Builder()
-    .forBrowser("chrome")
-    .setChromeOptions(new chrome.Options().headless())
+    .forBrowser("firefox")
+    .setFirefoxOptions(new firefox.Options().headless())
     .build();
   console.log("Loading Browser");
   await driver.get(`http://localhost:3000`);
@@ -40,25 +40,5 @@ const test = async () => {
     By.css(".listedArticle")
   );
   console.log(listedItemsUpdated.length == listedItems.length - 1);
-  //   await driver.switchTo().alert().accept();
-
-  //   console.log("3) Add new task");
-  //   let added_tasks = await driver.findElements(By.css(".added-tasks"));
-  //   const field = await driver.findElement(By.css(".add-task-input"));
-  //   await field.sendKeys("New Task");
-  //   await driver.findElement(By.css(".add-btn")).click();
-  //   added_tasksUpdated = await driver.findElements(By.css(".added-tasks"));
-  //   console.log(added_tasksUpdated.length == added_tasks.length + 1);
-
-  //   console.log("4) Verify newely added task");
-  //   added_tasks = await driver.findElements(By.css(".added-tasks-input"));
-  //   console.log(
-  //     (await added_tasks[added_tasks.length - 1].getText()) == "New Task"
-  //   );
-
-  //   console.log("5) Remove New Added Tasks");
-  //   await driver.findElement(By.css(".remove-task")).click();
-  //   added_tasks = await driver.findElements(By.css(".added-tasks-input"));
-  //   console.log(added_tasks.length == 0);
 };
 test();
